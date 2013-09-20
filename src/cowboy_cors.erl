fc40961c194b357ef3e6c03240743bbfc4599606
Cameron Bytheway <cameron@theflokk.com>
2013-09-20 12:32:42 -0400

    initial commit

---
+++ src/cowboy_cors.erl
@@ -0,0 +1,24 @@
+-module(cowboy_cors).
+
+-export([execute/2]).
+
+execute(Req, Env) ->
+  case fast_key:get(cors, Env) of
+    undefined ->
+      {ok, Req, Env};
+    Conf ->
+      Req2 = apply_headers([
+        {origin, <<"access-control-allow-origin">>, <<"*">>},
+        {headers, <<"access-control-allow-headers">>, <<"origin, x-requested-with, authorization, content-type, cache-control">>},
+        {methods, <<"access-control-allow-methods">>, <<"GET, POST, PUT, DELETE, HEAD">>},
+        {max_age, <<"access-control-max-age">>, <<"31556926">>}
+      ], Conf, Req),
+      {ok, Req2, Env}
+  end.
+
+apply_headers([], _, Req) ->
+  Req;
+apply_headers([{Prop, Header, Default}|Headers], Conf, Req) ->
+  Value = fast_key:get(Prop, Conf, Default),
+  Req2 = cowboy_req:set_resp_header(Header, Value, Req),
+  apply_headers(Headers, Conf, Req2).
