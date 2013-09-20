b0dace59de8368e77d7b5805b0fb19866876f98e
Cameron Bytheway <cameron@theflokk.com>
2013-09-20 16:07:07 -0400

    export init/1

--- src/cowboy_cors.erl
+++ src/cowboy_cors.erl
@@ -1,24 +1,34 @@
 -module(cowboy_cors).
 
 -export([execute/2]).
+-export([init/1]).
 
 execute(Req, Env) ->
   case fast_key:get(cors, Env) of
     undefined ->
       {ok, Req, Env};
-    Conf ->
-      Req2 = apply_headers([
-        {origin, <<"access-control-allow-origin">>, <<"*">>},
-        {headers, <<"access-control-allow-headers">>, <<"origin, x-requested-with, authorization, content-type, cache-control">>},
-        {methods, <<"access-control-allow-methods">>, <<"GET, POST, PUT, DELETE, HEAD">>},
-        {max_age, <<"access-control-max-age">>, <<"31556926">>}
-      ], Conf, Req),
-      {ok, Req2, Env}
+    Options ->
+      Fun = init(Options),
+      Fun(Req, Env)
   end.
 
-apply_headers([], _, Req) ->
+init(Options) ->
+  Origin = fast_key:get(origin, Options, <<"*">>),
+  Headers = fast_key:get(headers, Options, <<"origin, x-requested-with, authorization, content-type, cache-control">>),
+  Methods = fast_key:get(methods, Options, <<"GET, POST, PUT, DELETE, HEAD">>),
+  MaxAge = fast_key:get(max_age, Options, <<"31556926">>),
+  fun (Req, Env) ->
+    Req2 = apply_headers([
+      {<<"access-control-allow-origin">>, Origin},
+      {<<"access-control-allow-headers">>, Headers},
+      {<<"access-control-allow-methods">>, Methods},
+      {<<"access-control-max-age">>, MaxAge}
+    ], Req),
+    {ok, Req2, Env}
+  end.
+
+apply_headers([], Req) ->
   Req;
-apply_headers([{Prop, Header, Default}|Headers], Conf, Req) ->
-  Value = fast_key:get(Prop, Conf, Default),
+apply_headers([{Header, Value}|Headers], Req) ->
   Req2 = cowboy_req:set_resp_header(Header, Value, Req),
-  apply_headers(Headers, Conf, Req2).
+  apply_headers(Headers, Req2).
